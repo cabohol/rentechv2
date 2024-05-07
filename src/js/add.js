6b3040ef02a12f979ef9994e4142571aae0a1a5f
Ddana <161744609+Ddannaa@users.noreply.github.com>
2024-05-07 23:43:39 +0800

    naa lang gamay changes sa add

--- src/js/add.js
+++ src/js/add.js
@@ -12,28 +12,28 @@ const itemsImageUrl =
   
     const formData = new FormData(form_add);
 
-      console.log(formData.get("image_path"));
+  // console.log(formData.get("image_path"));
       const image = formData.get("image_path");
       const { data: storageData, error: storageError } = await supabase.storage
-        .from('laptops')
-        .upload('public/' + image.name, image, {
+    .from("laptops")
+    .upload("public/" + image.name, image, {
          cacheControl: "3600",
         upsert: true,
         });
      
-      console.log(storageData);
+  // console.log(storageData);
 
     const image_data = storageData;
 
     const { data, error } = await supabase
-      .from('laptops')
+    .from("laptops")
       .insert([
         {
           model: formData.get("model"),
           price: formData.get("price"),
           specs: formData.get("specs"),
           condition: formData.get("condition"),
-          image_path: image_data == null ? null:image_data.path,
+        image_path: image_data == null ? null : image_data.path,
           userinformation_id: userId, // Use the retrieved user's ID here
         },
       ])
@@ -56,7 +56,9 @@ async function getDatas() {
     container += `
     <div class="col">
       <div class="card" id="cards" data-id="${data.id}" >
-        <img src="${ itemsImageUrl + data.image_path}" class="card-img-top pt-2 mx-auto" alt="...">
+        <img src="${
+          itemsImageUrl + data.image_path
+        }" class="card-img-top pt-2 mx-auto" alt="...">
         <div class="card-body">
           <div class="row text-center">
             <h3 class="card-title">${data.model}</h3>
